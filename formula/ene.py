398bb0e7fe70ce700a9bd347b4331578a92f69c5
left-atrium <tao_90@163.com>
2016-10-13 14:34:04 +0800

    删除ene

--- formula/ene.py
+++
@@ -1,6 +0,0 @@
-from ma import ma
-def ene(stock_data,N=11,M1=10.0,M2=9.0):
-	stock_data['upper']=(1+M1/100.0) * ma(stock_data,N)['ma']
-	stock_data['lower']=(1-M2/100.0) * ma(stock_data,N)['ma']
-	stock_data['ene']=(stock_data['upper']+stock_data['lower'])/2
-	return stock_data
